be18ad33293f3172bd1d1368f37622e64ae76b5c
Kevin Zhang <kzhang8850@yahoo.com>
2016-04-21 01:34:29 -0400

    Finalized Technical Review 2 Prep and Framing Document

--- OpenGLTest2.py
+++ OpenGLTest2.py
@@ -16,10 +16,16 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import cv2
 import numpy
+import Image
 
+i=0
+imagecam = None
 #Creates the window for pyglet to render everything in, also initializes the camera
-window = pyglet.window.Window(800,600)
+window = pyglet.window.Window(fullscreen=True)
 camera = cv2.VideoCapture(0)
+temp = pyglet.image.load('immortalsfeat.jpg')
+sprite = pyglet.sprite.Sprite(temp)
+
 
 
 """
@@ -55,11 +61,41 @@ Work in Progress - Cube
 
 # glTranslatef(0.0,0.0, -5)
 
+
+
+def get_scale(window, image):
+	""" This function takes in the window and the image to be drawn
+	and determines the correct size 
+	"""
+	if image.width > image.height:
+		scale = float(window.width) / image.width
+	else:
+		scale = float(window.height) / image.height
+	return scale
 def update(dt):
 	"""
 	WTF is this function
 	"""
-	pass
+	global i, imagecam
+	
+
+	retval,img = camera.read()     #reads camera
+
+
+	# img2.save('temp.png')
+
+	print 'frame{}'.format(i)
+	i+=1
+
+	imagecam = Image.fromarray(img, 'RGB')
+
+	sprite.image = imagecam
+	sprite.scale = get_scale(window,imagecam)
+	sprite.x = 0
+	sprite.y = 0
+
+	window.clear()         #clears previous stuff on window
+
 	
 #window.event required by pyglet to run functions based on events/continually
 @window.event
@@ -81,20 +117,28 @@ def on_draw():
 	Overlays a Cube on top of the frame
 	"""
 
-	window.clear()         #clears previous stuff on window
-	retval,img = camera.read()     #reads camera
-	img = cv2.flip(img, 0)         #flips frame because for some reason it comes upside down
-	img = cv2.resize(img, (800, 600))   #resizes image
-	sy,sx,number_of_channels = img.shape    #divides data into matrix dimensions
-	number_of_bytes = sy*sx*number_of_channels    #transforms matrix into an array of data
 
-	img = img.ravel()   #idk
 
-	image_texture = (GLubyte * number_of_bytes)( *img.astype('uint8') )  #converts data into OpenGL format with GLubyte
+	sprite.draw()
 
-	pImg = pyglet.image.ImageData(sx, sy, 'BGR', image_texture, pitch = sx*number_of_channels)   #creates new texture render with formatted data
-	pImg.blit(0,0)    #puts new image thing onto window
+	# img = cv2.flip(img, 0)         #flips frame because for some reason it comes upside down
+	# img = cv2.resize(img, (800, 600))   #resizes image
+	# sy,sx,number_of_channels = img.shape    #divides data into matrix dimensions
+	# number_of_bytes = sy*sx*number_of_channels    #transforms matrix into an array of data
+	# img_2d = img
+	# img = img.ravel()   #idk
 
+
+
+
+	# image_texture = (GLubyte * number_of_bytes)( *img )  #converts data into OpenGL format with GLubyte
+
+	# pImg = pyglet.image.ImageData(sx, sy, 'BGR', image_texture, pitch = sx*number_of_channels)   #creates new texture render with formatted data
+	# pImg.blit(0,0)    #puts new image thing onto window
+
+
+	# cv2.imshow('my_image', img)
+	# cv2.waitKey(5)
 
 	"""
 	Work in Progress - Cube
@@ -111,20 +155,20 @@ def on_draw():
 
 
 	#Creates a colorful square to be rendered on top of the frame
-	glBegin(GL_QUADS)
-	glColor3f(1,0,0)
-	glVertex2i(400,400)
-	glColor3f(0,1,0)
-	glVertex2i(400,200)
-	glColor3f(0,0,1)
-	glVertex2i(200,200)
-	glColor3f(0,1,1)
-	glVertex2i(200,400)
-	glColor3f(1,1,1)
-	glEnd()
-	cv2.waitKey(1)
+	# glBegin(GL_QUADS)
+	# glColor3f(1,0,0)
+	# glVertex2i(400,400)
+	# glColor3f(0,1,0)
+	# glVertex2i(400,200)
+	# glColor3f(0,0,1)
+	# glVertex2i(200,200)
+	# glColor3f(0,1,1)
+	# glVertex2i(200,400)
+	# glColor3f(1,1,1)
+	# glEnd()
+	# cv2.waitKey(1)
 
 
 
-pyglet.clock.schedule(update)     #creates a timer that runs update which runs on_draw at set intervals
+pyglet.clock.schedule_interval(update, .01)     #creates a timer that runs update which runs on_draw at set intervals
 pyglet.app.run()   #launches the program and starts processing of window events, timers, etc.
